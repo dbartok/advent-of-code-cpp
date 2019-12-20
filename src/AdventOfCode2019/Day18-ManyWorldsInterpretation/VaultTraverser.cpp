5816ae7dc66eac23b59987623797c37f80a533b0
dbartok <34172998+dbartok@users.noreply.github.com>
2019-12-20 23:09:05 +0000

    Refactor Day18 - ManyWorldsInterpretation

---
+++ src/AdventOfCode2019/Day18-ManyWorldsInterpretation/VaultTraverser.cpp
@@ -0,0 +1,165 @@
+#include "VaultTraverser.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include <queue>
+__END_LIBRARIES_DISABLE_WARNINGS
+
+namespace AdventOfCode
+{
+
+struct VaultSearchNodeHash
+{
+    std::size_t operator()(const VaultSearchNode& node) const
+    {
+        std::size_t seed = 0;
+
+        boost::hash_combine(seed, node.x);
+        boost::hash_combine(seed, node.y);
+        boost::hash_combine(seed, node.keys.to_ulong());
+
+        return seed;
+    }
+};
+
+VaultTraverser::VaultTraverser(VaultMap vaultMap)
+    : m_map{std::move(vaultMap)}
+{
+    for (int j = 0; j < m_map.size(); ++j)
+    {
+        for (int i = 0; i < m_map.at(j).size(); ++i)
+        {
+            char tile = m_map.at(j).at(i);
+            if (isKey(tile))
+            {
+                m_allAvailableKeys.insert(tile);
+            }
+        }
+    }
+}
+
+void VaultTraverser::traverseAllKeys()
+{
+    std::queue<VaultSearchNode> bfsQueue;
+    VaultSearchNode initialNode = createInitialNode();
+    bfsQueue.push(initialNode);
+
+    std::unordered_set<VaultSearchNode, VaultSearchNodeHash> visited;
+    visited.insert(initialNode);
+
+    while (!bfsQueue.empty())
+    {
+        VaultSearchNode currentNode = bfsQueue.front();
+        bfsQueue.pop();
+
+        if (currentNode.keys.count() == m_allAvailableKeys.size())
+        {
+            m_shortestPathLength = currentNode.numSteps;
+            return;
+        }
+
+        std::vector<VaultSearchNode> neighborNodes = createNeighborNodes(currentNode);
+
+        for (auto& node : neighborNodes)
+        {
+            if (visited.find(node) == visited.cend())
+            {
+                visited.insert(node);
+                bfsQueue.push(node);
+            }
+        }
+    }
+
+    throw std::runtime_error("Unable to find all keys.");
+}
+
+int VaultTraverser::getShortestPathLength() const
+{
+    return m_shortestPathLength;
+}
+
+VaultSearchNode VaultTraverser::createInitialNode()
+{
+    for (int j = 0; j < m_map.size(); ++j)
+    {
+        for (int i = 0; i < m_map.at(j).size(); ++i)
+        {
+            if (m_map.at(j).at(i) == '@')
+            {
+                m_map.at(j).at(i) = '.';
+                return VaultSearchNode{i, j, 0, {}};
+            }
+        }
+    }
+
+    throw std::runtime_error("Unable to create initial state.");
+}
+
+std::vector<VaultSearchNode> VaultTraverser::createNeighborNodes(const VaultSearchNode& currentNode) const
+{
+    std::vector<VaultSearchNode> neighborNodes;
+    std::vector<VaultSearchNode> spatialNeighborNodes = createSpatialNeighborNodes(currentNode);
+
+    for (auto& spatialNeighborNode : spatialNeighborNodes)
+    {
+        char tile = m_map.at(spatialNeighborNode.y).at(spatialNeighborNode.x);
+        if (isKey(tile))
+        {
+            spatialNeighborNode.keys.set(tile - 'a');
+        }
+
+        if (!isObstructed(tile, spatialNeighborNode.keys))
+        {
+            neighborNodes.push_back(spatialNeighborNode);
+        }
+    }
+
+    return neighborNodes;
+}
+
+std::vector<VaultSearchNode> VaultTraverser::createSpatialNeighborNodes(const VaultSearchNode& currentNode) const
+{
+    std::vector<VaultSearchNode> spatialNeighborNodes;
+    spatialNeighborNodes.push_back(VaultSearchNode{currentNode.x + 1, currentNode.y, currentNode.numSteps + 1, currentNode.keys});
+    spatialNeighborNodes.push_back(VaultSearchNode{currentNode.x - 1, currentNode.y, currentNode.numSteps + 1, currentNode.keys});
+    spatialNeighborNodes.push_back(VaultSearchNode{currentNode.x, currentNode.y + 1, currentNode.numSteps + 1, currentNode.keys});
+    spatialNeighborNodes.push_back(VaultSearchNode{currentNode.x, currentNode.y - 1, currentNode.numSteps + 1, currentNode.keys});
+    return spatialNeighborNodes;
+}
+
+bool VaultTraverser::isKey(char tile) const
+{
+    return tile >= 'a' && tile <= 'z';
+}
+
+bool VaultTraverser::isObstructed(char tile, const std::bitset<ALPHABET_LENGTH>& keys) const
+{
+    if (tile == '#')
+    {
+        return true;
+    }
+
+    if (isDoor(tile))
+    {
+        char key = getKeyForDoor(tile);
+        if (m_allAvailableKeys.find(key) != m_allAvailableKeys.cend() && !keys.test(key - 'a'))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool VaultTraverser::isDoor(char tile) const
+{
+    return tile >= 'A' && tile <= 'Z';
+}
+
+char VaultTraverser::getKeyForDoor(char door) const
+{
+    return std::tolower(door);
+}
+
+}
---
+++ src/AdventOfCode2019/Day18-ManyWorldsInterpretation/VaultTraverser.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include <boost/functional/hash/hash.hpp>
+
+#include <bitset>
+#include <unordered_set>
+__END_LIBRARIES_DISABLE_WARNINGS
+
+namespace
+{
+const size_t ALPHABET_LENGTH = 26;
+}
+
+
+namespace AdventOfCode
+{
+
+using VaultMap = std::vector<std::string>;
+
+struct VaultSearchNode
+{
+    int x;
+    int y;
+    int numSteps;
+    std::bitset<ALPHABET_LENGTH> keys;
+
+    bool operator==(const VaultSearchNode& other) const
+    {
+        return x == other.x && y == other.y && keys == other.keys;
+    }
+};
+
+class VaultTraverser
+{
+public:
+    VaultTraverser(VaultMap vaultMap);
+
+    void traverseAllKeys();
+
+    int getShortestPathLength() const;
+
+private:
+    VaultMap m_map;
+    std::unordered_set<char> m_allAvailableKeys;
+
+    int m_shortestPathLength = std::numeric_limits<int>::max();
+
+    VaultSearchNode createInitialNode();
+
+    std::vector<VaultSearchNode> createNeighborNodes(const VaultSearchNode& currentNode) const;
+    std::vector<VaultSearchNode> createSpatialNeighborNodes(const VaultSearchNode& currentNode) const;
+    bool isKey(char tile) const;
+    bool isObstructed(char tile, const std::bitset<ALPHABET_LENGTH>& keys) const;
+    bool isDoor(char tile) const;
+    char getKeyForDoor(char door) const;
+};
+
+}
