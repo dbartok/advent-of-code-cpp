146eadb94af61c3f6d093769a82487c1680c1fd9
dbartok <34172998+dbartok@users.noreply.github.com>
2022-12-07 21:48:49 +0000

    Add Day7 - NoSpaceLeftOnDevice (part 1)

---
+++ src/AdventOfCode2022/Day07-NoSpaceLeftOnDevice/Day07-Main.cpp
@@ -0,0 +1,26 @@
+#include "Day07-NoSpaceLeftOnDevice.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+__END_LIBRARIES_DISABLE_WARNINGS
+
+int main()
+{
+    namespace CurrentDay = AdventOfCode::Year2022::Day07;
+
+    std::fstream fileIn("input.txt");
+
+    std::string lineBuffer;
+    std::vector<std::string> lines;
+    while (std::getline(fileIn, lineBuffer))
+    {
+        lines.push_back(std::move(lineBuffer));
+    }
+
+    std::cout << "First part: " << CurrentDay::totalSizeOfSmallDirectories(lines) << std::endl;
+}
---
+++ src/AdventOfCode2022/Day07-NoSpaceLeftOnDevice/test_Day07-NoSpaceLeftOnDevice.cpp
@@ -0,0 +1,48 @@
+#include "Day07-NoSpaceLeftOnDevice.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include "CppUnitTest.h"
+__END_LIBRARIES_DISABLE_WARNINGS
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace CurrentDay = AdventOfCode::Year2022::Day07;
+
+TEST_CLASS(Day07NoSpaceLeftOnDevice)
+{
+public:
+
+    TEST_METHOD(totalSizeOfSmallDirectories_SimpleTests)
+    {
+        const std::vector<std::string> lines =
+        {
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k"
+        };
+
+        Assert::AreEqual(95437, CurrentDay::totalSizeOfSmallDirectories(lines));
+    }
+
+};
