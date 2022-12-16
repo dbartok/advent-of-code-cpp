4cd958c864130df0ea2e13a31930bda8adbec9a0
dbartok <34172998+dbartok@users.noreply.github.com>
2022-12-16 14:39:26 +0000

    Add Day16 - ProboscideaVolcanium (part 1)

---
+++ src/AdventOfCode2022/Day16-ProboscideaVolcanium/Day16-ProboscideaVolcanium.cpp
@@ -0,0 +1,328 @@
+#include "Day16-ProboscideaVolcanium.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include <boost/algorithm/string.hpp>
+#include <boost/functional/hash.hpp>
+
+#include <regex>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <bitset>
+__END_LIBRARIES_DISABLE_WARNINGS
+
+namespace
+{
+
+const char* START_ROOM_NAME = "AA";
+const size_t START_ROOM_INDEX = 0;
+int AVAILABLE_TIME = 30;
+const size_t MAX_NUM_RELEVANT_ROOMS = 75;
+
+}
+
+namespace AdventOfCode
+{
+namespace Year2022
+{
+namespace Day16
+{
+
+struct ValveRoom
+{
+    std::string name;
+    int flowRate;
+    std::vector<std::string> connectedRoomNames;
+
+};
+
+using CostMatrixRow = std::vector<int>;
+using CostMatrix = std::vector<CostMatrixRow>;
+
+class ValveNetworkSimplifier
+{
+public:
+    ValveNetworkSimplifier(std::vector<ValveRoom> valveRooms)
+    {
+        for (auto& room : valveRooms)
+        {
+            m_roomNameToRoom.emplace(room.name, std::move(room));
+        }
+    }
+
+    void buildCostMatrix()
+    {
+        allocateCostMatrixIndices();
+
+        const size_t numRooms = m_roomNameToRoom.size();
+
+        m_costMatrix = CostMatrix(numRooms, CostMatrixRow(numRooms));
+
+        for (const auto& roomNameAndRoom : m_roomNameToRoom)
+        {
+            buildCostMatrixRow(roomNameAndRoom.first);
+        }
+    }
+
+    CostMatrix getCostMatrix() const
+    {
+        return m_costMatrix;
+    }
+
+    std::unordered_map<size_t, int> getRoomIndexToFlowRate() const
+    {
+        std::unordered_map<size_t, int> roomIndexToFlowRate;
+
+        for (const auto& roomNameAndRoom : m_roomNameToRoom)
+        {
+            const size_t roomIndex = m_roomNameToCostMatrixIndex.at(roomNameAndRoom.first);
+            roomIndexToFlowRate.emplace(roomIndex, roomNameAndRoom.second.flowRate);
+        }
+
+        return roomIndexToFlowRate;
+    }
+
+private:
+    std::unordered_map<std::string, ValveRoom> m_roomNameToRoom;
+
+    std::unordered_map<std::string, size_t> m_roomNameToCostMatrixIndex;
+    CostMatrix m_costMatrix;
+
+    void allocateCostMatrixIndices()
+    {
+        m_roomNameToCostMatrixIndex.emplace(START_ROOM_NAME, START_ROOM_INDEX);
+
+        size_t currentIndex = 1;
+
+        for (const auto& roomNameAndRoom : m_roomNameToRoom)
+        {
+            const bool wasInserted = m_roomNameToCostMatrixIndex.emplace(roomNameAndRoom.first, currentIndex).second;
+            if (wasInserted)
+            {
+                ++currentIndex;
+            }
+        }
+    }
+
+    void buildCostMatrixRow(const std::string& startRoomName)
+    {
+        const size_t startRoomIndex = m_roomNameToCostMatrixIndex.at(startRoomName);
+
+        std::queue<std::string> roomNamesBfsQueue;
+        roomNamesBfsQueue.push(startRoomName);
+
+        while (!roomNamesBfsQueue.empty())
+        {
+            std::string currentRoomName = roomNamesBfsQueue.front();
+            roomNamesBfsQueue.pop();
+
+            const auto& currentRoom = m_roomNameToRoom.at(currentRoomName);
+            const size_t currentRoomIndex = m_roomNameToCostMatrixIndex.at(currentRoomName);
+            const int currentRoomCost = m_costMatrix.at(startRoomIndex).at(currentRoomIndex);
+
+            for (const auto& connectedRoomName : currentRoom.connectedRoomNames)
+            {
+                const size_t connectedRoomIndex = m_roomNameToCostMatrixIndex.at(connectedRoomName);
+
+                if (startRoomIndex != connectedRoomIndex && m_costMatrix.at(startRoomIndex).at(connectedRoomIndex) == 0)
+                {
+                    m_costMatrix.at(startRoomIndex).at(connectedRoomIndex) = currentRoomCost + 1;
+                    roomNamesBfsQueue.push(connectedRoomName);
+                }
+            }
+        }
+    }
+};
+
+class FlowRateOptimizer
+{
+public:
+    FlowRateOptimizer(CostMatrix costMatrix, std::unordered_map<size_t, int> roomIndexToFlowRate)
+        : m_costMatrix{std::move(costMatrix)}
+        , m_roomIndexToFlowRate{std::move(roomIndexToFlowRate)}
+    {
+        for (const auto& roomIndexAndFlowRate : m_roomIndexToFlowRate)
+        {
+            if (roomIndexAndFlowRate.second != 0)
+            {
+                m_roomIndicesWithNonZeroFlowRate.push_back(roomIndexAndFlowRate.first);
+            }
+        }
+    }
+
+    void createAllStates()
+    {
+        StateSet openStates;
+        State startState{START_ROOM_INDEX, AVAILABLE_TIME, {}, 0};
+        openStates.insert(startState);
+
+        while (!openStates.empty())
+        {
+            StateSet nextOpenStates;
+
+            for (const auto& currentState : openStates)
+            {
+                std::vector<State> nextStates = getNextStates(currentState);
+                if (nextStates.empty())
+                {
+                    insertKeepingHigherTotalPressureReleased(m_closedStates, currentState);
+                }
+
+                for (auto& nextState : nextStates)
+                {
+                    insertKeepingHigherTotalPressureReleased(nextOpenStates, nextState);
+                }
+            }
+
+            openStates = nextOpenStates;
+        }
+    }
+
+    int getMaxPressureReleased() const
+    {
+        const auto maxPressureReleasedStateIter = std::max_element(m_closedStates.cbegin(), m_closedStates.cend(), [](const auto& lhs, const auto& rhs)
+                                                                   {
+                                                                       return lhs.totalPressureReleased < rhs.totalPressureReleased;
+                                                                   });
+
+        return maxPressureReleasedStateIter->totalPressureReleased;
+    }
+
+private:
+    struct State
+    {
+        size_t currentRoomIndex;
+        int remainingTime;
+        std::bitset<MAX_NUM_RELEVANT_ROOMS> visitedRoomIndices;
+
+        int totalPressureReleased;
+
+        bool operator==(const State& other) const
+        {
+            return currentRoomIndex == other.currentRoomIndex &&
+                remainingTime == other.remainingTime &&
+                visitedRoomIndices == other.visitedRoomIndices;
+        }
+    };
+
+    struct StateHash
+    {
+        std::size_t operator()(const State& state) const
+        {
+            std::size_t seed = 0;
+
+            boost::hash_combine(seed, state.currentRoomIndex);
+            boost::hash_combine(seed, state.remainingTime);
+            boost::hash_combine(seed, state.visitedRoomIndices.to_ullong());
+
+            return seed;
+        }
+    };
+
+    using StateSet = std::unordered_set<State, StateHash>;
+
+    CostMatrix m_costMatrix;
+    std::unordered_map<size_t, int> m_roomIndexToFlowRate;
+    std::vector<size_t> m_roomIndicesWithNonZeroFlowRate;
+
+    StateSet m_closedStates;
+
+    std::vector<State> getNextStates(const State& currentState) const
+    {
+        std::vector<State> nextStates;
+
+        for (const size_t nextRoomIndex : m_roomIndicesWithNonZeroFlowRate)
+        {
+            if (currentState.visitedRoomIndices.test(nextRoomIndex))
+            {
+                continue;
+            }
+
+            const int distanceToNextRoom = m_costMatrix.at(currentState.currentRoomIndex).at(nextRoomIndex);
+            const int remainingTimeAfterNextValveOpen = currentState.remainingTime - distanceToNextRoom - 1;
+
+            // No time for any newly opened valve to release pressure
+            if (remainingTimeAfterNextValveOpen < 1)
+            {
+                continue;
+            }
+
+            auto newVisitedRoomIndices{currentState.visitedRoomIndices};
+            newVisitedRoomIndices.set(nextRoomIndex);
+
+            const int currentRoomFlowRate = m_roomIndexToFlowRate.at(nextRoomIndex);
+            const int newTotalPressureReleased = currentState.totalPressureReleased + remainingTimeAfterNextValveOpen * currentRoomFlowRate;
+
+            State nextState{nextRoomIndex, remainingTimeAfterNextValveOpen, newVisitedRoomIndices, newTotalPressureReleased};
+
+            nextStates.push_back(std::move(nextState));
+        }
+
+        return nextStates;
+    }
+
+    static void insertKeepingHigherTotalPressureReleased(StateSet& states, State newState)
+    {
+        auto insertionResult = states.insert(newState);
+        if (!insertionResult.second)
+        {
+            auto existingStateIter = insertionResult.first;
+            if (newState.totalPressureReleased > existingStateIter->totalPressureReleased)
+            {
+                states.erase(existingStateIter);
+                states.insert(std::move(newState));
+            }
+        }
+    }
+};
+
+ValveRoom parseValveRoomLine(const std::string& valveRoomLine)
+{
+    const std::regex valveRoomLineRegex(R"(Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? ([A-Z,\s]+))");
+    std::smatch valveRoomLineMatch;
+
+    std::regex_match(valveRoomLine, valveRoomLineMatch, valveRoomLineRegex);
+
+    std::string name = valveRoomLineMatch[1];
+    const int flowRate = std::stoi(valveRoomLineMatch[2]);
+
+    std::vector<std::string> connectedRoomNames;
+    boost::split(connectedRoomNames, std::string{valveRoomLineMatch[3]}, boost::is_any_of(" ,"), boost::token_compress_on);
+
+    return ValveRoom{std::move(name), flowRate, std::move(connectedRoomNames)};
+}
+
+std::vector<ValveRoom> parseValveRoomLines(const std::vector<std::string>& valveRoomLines)
+{
+    std::vector<ValveRoom> valveRooms;
+
+    for (const auto& valveRoomLine : valveRoomLines)
+    {
+        ValveRoom valveRoom = parseValveRoomLine(valveRoomLine);
+        valveRooms.push_back(std::move(valveRoom));
+    }
+
+    return valveRooms;
+}
+
+int mostPressureThatCanBeReleased(const std::vector<std::string>& valveRoomLines)
+{
+    std::vector<ValveRoom> valveRooms = parseValveRoomLines(valveRoomLines);
+
+    ValveNetworkSimplifier valveNetworkSimplifier{std::move(valveRooms)};
+    valveNetworkSimplifier.buildCostMatrix();
+
+    CostMatrix costMatrix = valveNetworkSimplifier.getCostMatrix();
+    std::unordered_map<size_t, int> roomIndexToFlowRate = valveNetworkSimplifier.getRoomIndexToFlowRate();
+
+    FlowRateOptimizer flowRateOptimizer{std::move(costMatrix), std::move(roomIndexToFlowRate)};
+    flowRateOptimizer.createAllStates();
+
+    return flowRateOptimizer.getMaxPressureReleased();
+}
+
+}
+}
+}
---
+++ src/AdventOfCode2022/Day16-ProboscideaVolcanium/test_Day16-ProboscideaVolcanium.cpp
@@ -0,0 +1,35 @@
+#include "Day16-ProboscideaVolcanium.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include "CppUnitTest.h"
+__END_LIBRARIES_DISABLE_WARNINGS
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace CurrentDay = AdventOfCode::Year2022::Day16;
+
+TEST_CLASS(Day16ProboscideaVolcanium)
+{
+public:
+
+    TEST_METHOD(mostPressureThatCanBeReleased_SimpleTests)
+    {
+        const std::vector<std::string> lines =
+        {
+            "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+            "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
+            "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
+            "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
+            "Valve EE has flow rate=3; tunnels lead to valves FF, DD",
+            "Valve FF has flow rate=0; tunnels lead to valves EE, GG",
+            "Valve GG has flow rate=0; tunnels lead to valves FF, HH",
+            "Valve HH has flow rate=22; tunnel leads to valve GG",
+            "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
+            "Valve JJ has flow rate=21; tunnel leads to valve II"
+        };
+
+        Assert::AreEqual(1651, CurrentDay::mostPressureThatCanBeReleased(lines));
+    }
+
+};
