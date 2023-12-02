cdcef81f39e74ca940af959603c29f24f4679f90
dbartok <34172998+dbartok@users.noreply.github.com>
2023-12-02 15:32:51 +0000

    Add Day2 - CubeConundrum (part 1)

---
+++ src/AdventOfCode2023/Day02-CubeConundrum/Day02-CubeConundrum.cpp
@@ -0,0 +1,126 @@
+#include "Day02-CubeConundrum.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include <boost/algorithm/string.hpp>
+__END_LIBRARIES_DISABLE_WARNINGS
+
+namespace AdventOfCode
+{
+namespace Year2023
+{
+namespace Day02
+{
+
+struct CubeSubset
+{
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+
+    bool operator<=(const CubeSubset& other) const
+    {
+        return this->red <= other.red&& this->green <= other.green&& this->blue <= other.blue;
+    }
+};
+
+const CubeSubset PRESET_BAG = CubeSubset{12, 13, 14};
+
+using Game = std::vector<CubeSubset>;
+
+CubeSubset parseCubesRevealedString(const std::string& cubesRevealedString)
+{
+    std::vector<std::string> cubesRevealedTokens;
+    boost::split(cubesRevealedTokens, cubesRevealedString, boost::is_any_of(", "), boost::token_compress_on);
+
+    CubeSubset cubeSubset;
+
+    for (size_t i = 1; i < cubesRevealedTokens.size(); i+=2)
+    {
+        const int quantity = std::stoi(cubesRevealedTokens.at(i - 1));
+        const std::string color = cubesRevealedTokens.at(i);
+
+        if (color == "red")
+        {
+            cubeSubset.red = quantity;
+        }
+        else if (color == "green")
+        {
+            cubeSubset.green = quantity;
+        }
+        else if (color == "blue")
+        {
+            cubeSubset.blue = quantity;
+        }
+    }
+
+    return cubeSubset;
+}
+
+Game parseGameRecordLine(const std::string& gameRecordLine)
+{
+    std::vector<std::string> cubesRevealedStrings;
+    boost::split(cubesRevealedStrings, gameRecordLine, boost::is_any_of(";:"));
+
+    Game game;
+
+    for (size_t i = 1; i < cubesRevealedStrings.size(); ++i)
+    {
+        auto cubesRevealedString = std::move(cubesRevealedStrings.at(i));
+        boost::trim(cubesRevealedString);
+        CubeSubset cubesRevealed = parseCubesRevealedString(cubesRevealedString);
+        game.push_back(std::move(cubesRevealed));
+    }
+
+    return game;
+}
+
+std::vector<Game> parseGameRecordLines(const std::vector<std::string>& gameRecordLines)
+{
+    std::vector<Game> games;
+
+    for (const auto& gameRecordLine : gameRecordLines)
+    {
+        Game game = parseGameRecordLine(gameRecordLine);
+        games.push_back(std::move(game));
+    }
+
+    return games;
+}
+
+bool isValidSubsetOfPresetBag(const CubeSubset& cubeSubset)
+{
+    return cubeSubset <= PRESET_BAG;
+}
+
+bool isGamePossibleWithPresetBag(const Game& game)
+{
+    return std::all_of(game.cbegin(), game.cend(), [](const auto& cubeSubset)
+                       {
+                           return isValidSubsetOfPresetBag(cubeSubset);
+                       });
+}
+
+
+int sumOfIDsOfPossibleGames(const std::vector<std::string>& gameRecordLines)
+{
+    std::vector<Game> games = parseGameRecordLines(gameRecordLines);
+
+    int sum = 0;
+
+    for (int gameIndex = 0; gameIndex < games.size(); ++gameIndex)
+    {
+        if (isGamePossibleWithPresetBag(games.at(gameIndex)))
+        {
+            const int gameID = gameIndex + 1;
+            sum += gameID;
+        }
+    }
+
+    return sum;
+}
+
+}
+}
+}
---
+++ src/AdventOfCode2023/Day02-CubeConundrum/test_Day02-CubeConundrum.cpp
@@ -0,0 +1,30 @@
+#include "Day02-CubeConundrum.h"
+
+#include <AdventOfCodeCommon/DisableLibraryWarningsMacros.h>
+
+__BEGIN_LIBRARIES_DISABLE_WARNINGS
+#include "CppUnitTest.h"
+__END_LIBRARIES_DISABLE_WARNINGS
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace CurrentDay = AdventOfCode::Year2023::Day02;
+
+TEST_CLASS(Day02CubeConundrum)
+{
+public:
+
+    TEST_METHOD(sumOfIDsOfPossibleGames_SimpleTests)
+    {
+        const std::vector<std::string> lines =
+        {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        };
+
+        Assert::AreEqual(8, CurrentDay::sumOfIDsOfPossibleGames(lines));
+    }
+
+};
